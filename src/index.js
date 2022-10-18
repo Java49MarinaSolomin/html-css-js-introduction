9d0e940001f392f762780e0386c5e169dfa1dd92
MarinaSolomin <solomina.maryna@gmail.com>
2022-10-18 21:33:46 +0300

    fixing scroll bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+console.log("Launching script");
 const anchors=document.querySelectorAll(".thumbnails-anchor");
 const detailsImage=document.querySelector(".details-image");
 const detailsTitle=document.querySelector(".details-title");
