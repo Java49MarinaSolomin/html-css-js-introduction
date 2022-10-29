d599938c39d29569d332b9de4935e09e375ccda8
MarinaSolomin <solomina.maryna@gmail.com>
2022-10-29 22:47:49 +0300

    adding sounds

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ const anchors=document.querySelectorAll(".thumbnail-anchor");
 const detailsImage=document.querySelector(".details-image");
 const detailsTitle=document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
-const HIDDEN= "hidden";
+const detailsContainer = document.querySelector(".details-container");
+const audio = document.getElementById("audio");
+const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function setDetails (anchor){
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML= anchor.getAttribute('date-details-title');
+    audio.setAttribute("src", anchor.getAttribute('data-sound'));
+    audio.play();
+    setTimeout(function(){
+        audio.pause();
+        audio.currentTime = 0;
+    },10000)
 }
 
 for (let i=0; i< anchors.length; i++){
@@ -18,6 +27,10 @@ for (let i=0; i< anchors.length; i++){
 }
 function showDetails() {
 mainClass.classList.remove(HIDDEN);
+detailsContainer.classList.add(IS_POINT);
+setTimeout(function(){
+    detailsContainer.classList.remove(IS_POINT);
+},1)
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
