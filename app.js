5c21be4ed6f23525a923f397f4f5a42d706ce532
MarinaSolomin <solomina.maryna@gmail.com>
2022-11-01 21:12:25 +0200

    Homework 11

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-// console.log("Hello world");
-// for (var i=0; i<3; i++){
-// setTimeout(function(){console.log(i)});
-// }=> 3 3 3
 
-// for (let i=0; i<3; i++){
-// setTimeout(function(){console.log(i)});
-// } => 0 1 2
-
-// for (var i=0; i<3; i++){
-// console.log(i);
-// }
-// console.log(i);
-
-// for (var i=0; i<3; i++){
-//     setTimeout(function(){
-//         console.log(i)
-//     },5000);
-//      }
-//      i=100;
-     
-// function sum (op1,op2){
-//     let res= op1+op2;
-//     return res;
-// }
-// console.log(sum(10,20));
 //************************************************************** */
 // Homework 11
 // function sumDigits(number){
